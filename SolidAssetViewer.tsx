f4b2748296f2df8cb5015ec9e8a982a138c060c8
JanEuster <gh@jan.eusterschulte.com>
2023-02-01 16:57:13 +0100

    give previews hover title

--- SolidAssetViewer.tsx
+++ SolidAssetViewer.tsx
@@ -434,6 +434,7 @@ function SAVPreviews({
             ? " solid-asset-viewer-preview-selected"
             : "")
         }
+        title={asset.name}
         onClick={(e) => {
           setSelected(i);
           e.stopPropagation();
